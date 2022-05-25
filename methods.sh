0da812167b233a4394e75a192c860762c8c46343
Florin <83332450+flawreen@users.noreply.github.com>
2022-05-25 22:37:15 +0300

    Update methods.sh

--- methods.sh
+++ methods.sh
@@ -5,25 +5,25 @@ function view {
 }
 
 function add {
-	read -p "Type unic ID: " unicID
+	read -p "Type unique ID: " uniqueID
 	read -p "Owner's name: " name
 	read -p "House location(city): " city
 	read -p "House surface(sq meters): " sqm
 	read -p "Number of levels: " levels
 	echo -e "\nUnic_ID Name City Sqm Levels"
-	echo "$unicID $name $city $sqm $levels"
+	echo "$uniqueID $name $city $sqm $levels"
 	echo "Save data?"
 	read x
 	if [ $x == 'y' ]
 	then
-		echo "$unicID $name $city $sqm $levels" >> data
+		echo "$uniqueID $name $city $sqm $levels" >> data
 	fi
 }
 
 function search {
 	read -p "Owner's name: " name
 	echo "Properties owned by $name:"
-	echo -e "\nUnic_ID Name City Sqm Levels"
+	echo -e "\nUniqueID Name City Sqm Levels"
 	grep $name data
 }
 
